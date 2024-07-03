c528de5afac3b2384044706c23a1e6b40bd0088f
unknown <dev.juan.eloper@gmail.com>
2024-07-03 11:55:13 -0600

    Alias

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+export const Routes: React.FC = () => {
+    return (
+        <div>
+            <h1>Routes</h1>
+        </div>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const root = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "components": path.posix.join(root, 'components'),
+      "routes": path.posix.join(root, 'routes'),
+    },
+  },
 })
